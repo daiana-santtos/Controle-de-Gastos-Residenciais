ff8908b99be06ef1c09014851f2f917cbca719f0
Daiana <daiana.santtos03@gmail.com>
2026-07-19 17:35:05 -0300

    chore: setup project config, EF Core SQLite and domain models

---
+++ ControleGastos.Api/Models/TipoTransacao.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ControleGastos.Api.Models
+{
+    public enum TipoTransacao
+    {
+        Despesa = 1,
+        Receita = 2
+    }
+}
---
+++ ControleGastos.Api/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ControleGastos.Api.Data;
+using ControleGastos.Api.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<PessoaService>();
+builder.Services.AddScoped<TransacaoService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Controle de Gastos Residenciais")
+               .WithTheme(ScalarTheme.Purple) 
+               .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
